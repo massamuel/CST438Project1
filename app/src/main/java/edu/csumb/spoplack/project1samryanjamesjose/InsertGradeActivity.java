f5ac5db9457e25a26c4823aabf5fd96c6564ba7f
jdbarq <jamesbarquera@gmail.com>
2020-02-01 13:50:22 -0800

    First commit

--- app/src/main/java/edu/csumb/spoplack/project1samryanjamesjose/InsertGradeActivity.java
+++ app/src/main/java/edu/csumb/spoplack/project1samryanjamesjose/InsertGradeActivity.java
@@ -2,8 +2,15 @@ package edu.csumb.spoplack.project1samryanjamesjose;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class InsertGradeActivity extends AppCompatActivity {
+
+
+     private Button b1;
+     Button b2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
