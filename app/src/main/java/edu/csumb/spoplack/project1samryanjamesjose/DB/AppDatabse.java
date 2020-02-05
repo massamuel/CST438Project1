110ca23789c21d3738cc9304be5f44da0b4d11d0
ryan-beck <rbeck@csumb.edu>
2020-02-04 20:12:43 -0800

    fixing

--- app/src/main/java/edu/csumb/spoplack/project1samryanjamesjose/DB/AppDatabse.java
+++ app/src/main/java/edu/csumb/spoplack/project1samryanjamesjose/DB/AppDatabse.java
@@ -3,19 +3,20 @@ package edu.csumb.spoplack.project1samryanjamesjose.DB;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 
-import edu.csumb.spoplack.project1samryanjamesjose.;
+import edu.csumb.spoplack.project1samryanjamesjose.Database.User.User;
+
+;
 
 
 public class AppDatabse {
 
-//    @Database(entities = {User.class}, version=1)
-//    @TypeConverters(DateTypeConverter.class)
-//    public abstract class AppDatabase extends RoomDatabase {
-//        public static final String DBNAME = "db-gymlog";
-//        public static final String GYMLOG_TABLE = "gymlog";
-//        public abstract USER_DAO getGymLogDAO();
-//    }
+    @Database(entities = {User.class}, version=1)
+    @TypeConverters(DateTypeConverter.class)
+    public abstract class AppDatabase extends RoomDatabase {
+        public static final String DBNAME = "db-gymlog";
+        public static final String GYMLOG_TABLE = "gymlog";
+        public abstract USER_DAO getGymLogDAO();
+    }
 }
